b63478f00a622343ab97e8f1e5a0a3e23311164d
michaelnator <miengel05@protonmail.com>
2020-11-24 13:32:01 +0100

    initial commit client

---
+++ src/net/client/client.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h> 
+#include <pthread.h>
+#include <string.h>
+#include <errno.h>
+#include "client.h"
+
+/*TODO : give him a addr in client_init via char* or in the struct*/
+
+int client_connect(client_conn *client)
+{
+
+	if(connect(client->socket,(struct sockaddr*)&(client->local_client),sizeof(struct sockaddr_in)) < 0) {
+        perror("Unable to connect to socket:"); 
+        return -1;
+    }
+	return 0;
+}
+
+
+int client_init(client_conn *client, int port)
+{
+	/*intit local_client sockaddr... */ 
+
+	client->local_client.sin_family = AF_INET;
+	client->local_client.sin_port = htons(port);
+	client->local_client.sin_addr.s_addr = /*give him an ip somehow via char* or struct*/
+
+	if((client->socket = socket(AF_INET, SOCK_STREAM, 0)) <0)
+	{
+		perror("Unable to create socket:"); 
+		return -1;
+	}
+
+	if(client_connect(&client) < 0)
+	{
+		perror("Unable to connect to server:");
+		return -2;
+	}
+	return 0;
+
+}
---
+++ src/net/client/client.h
@@ -0,0 +1,17 @@
+#ifndef CLIENT_T
+#define CLIENT_T
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+typedef struct {
+	struct sockaddr_in local_client;
+	int socket;
+	/*how should we tell which ip/addr to use */
+	struct in_addr * addr;
+}client_conn;
+
+
+int client_connect(client_conn *client);
+int client_init(client_conn *client, int port);
+#endif
