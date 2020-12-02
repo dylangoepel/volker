41f1dfb0d5b64b4e27550c20f2f95bef4148d5bc
michaelnator <miengel05@protonmail.com>
2020-12-02 20:05:35 +0100

    add bool for better bruteforcing

--- src/net/ssh/ssh.c
+++ src/net/ssh/ssh.c
@@ -1,15 +1,41 @@
 #include <libssh/libssh.h>
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <stdbool.h> 
 
 /*
 
 TODO : scp func
 
  */
+
+
+bool
+ssh_try_pwd(char *pwd, ssh_session session){
+
+  int c;
+
+  if(session == NULL){
+    return false;
+  }
+
+  if((c = ssh_userauth_password(session, NULL, pwd)) != SSH_AUTH_SUCCESS){
+		//its maybe not the best way
+	    fprintf(stderr, "Error: %s\n", ssh_get_error(session));
+	    return false;
+  }
+
+  if(c == SSH_AUTH_SUCCESS){
+      return true;
+  }
+
+  
+  return false;
+
+}
+
 int
-_ssh_init(char *hstn, char *usr, char *pwd, ssh_session session){
+_ssh_init(char *hstn, char *usr, ssh_session session){
 
   session = ssh_new();
   if(session == NULL){
@@ -29,18 +55,6 @@ _ssh_init(char *hstn, char *usr, char *pwd, ssh_session session){
     ssh_free(session);
     return -2;
   }
-
-  if((c = ssh_userauth_password(session, NULL, pwd)) != SSH_AUTH_SUCCESS){
-    fprintf(stderr, "Error: %s\n", ssh_get_error(session));
-    ssh_disconnect(session);
-    ssh_free(session);
-    return -3;
-  }
-
-    /*checks if auth works*/
-    if(c == SSH_AUTH_SUCCESS){
-      return 1;
-    }
 
   return -4;
 }
--- src/net/ssh/ssh.h
+++ src/net/ssh/ssh.h
@@ -1,6 +1,8 @@
 #ifndef SSH_H
 #define SSH_H
 
+#include <libssh/libssh.h>
+#include <stdbool.h> 
 
 /* dont know if we should put the informations usr, pwd, hst in a struct or not, is takes the same time. 
    i think, direct in the function is prettier
@@ -8,7 +10,7 @@
 
 
 
-int _ssh_init(char *hstn, char *usr, char *pwd, ssh_session session);
-
+int _ssh_init(char *hstn, char *usr, ssh_session session);
+bool ssh_try_pwd(char *pwd, ssh_session session);
 
 #endif // SSH_H
