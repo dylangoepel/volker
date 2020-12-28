940efad9e13fc0ee1a7ca403b7b137ee82dd4e83
Dylan Göpel <dylan.connor@t-online.de>
2020-12-28 23:21:52 +0100

    rename and add more tpool benchmarks

--- benchmarks/tpool.c
+++ benchmarks/tpool.c
@@ -9,26 +9,29 @@
 #define MAX_QUEUE 40
 
 void aufg(void *arg){
-  int a = (int)arg;
+  int a = (int) arg;
   a++;
 }
 
 
 int main(){
-
   benchmark ben;
+  tpool_t test;
 
   bm_start(&ben);
-  
-  tpool_t test;
   tpool_init(&test, NUM_THREADS, MAX_QUEUE);
+  bm_end(&ben);
+  bm_write(&ben, "tpool_init(20)");
 
+  bm_start(&ben);
   for(int i = 0; i < 30; ++i){
     tpool_add_work(test, aufg, (void *)(uintptr_t)i);
   }
-
-  tpool_destroy(test, 1);
-
   bm_end(&ben);
-  bm_write(&ben, "created tpool with 20 Threads ");
+  bm_write(&ben, "tpool_add_work(20)");
+
+  bm_start(&ben);
+  tpool_destroy(test, 1);
+  bm_end(&ben);
+  bm_write(&ben, "tpool_destroy");
 }
