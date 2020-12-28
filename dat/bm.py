66a245357ad96c58342d6f28ae293b542f55d32a
Dylan Göpel <dylan.connor@t-online.de>
2020-12-28 23:21:34 +0100

    add support for multi benchmarking in bm.py

--- dat/bm.py
+++ dat/bm.py
@@ -1,13 +1,34 @@
 import os
 import matplotlib.pyplot as plt
 
-di = {}
+def readDat(path):
+    ret = {}
+    counts = {}
+    for line in open(path, "r").readlines():
+        row = line.split("\t")
+        if row[0] not in ret:
+            ret[row[0]] = int(row[1])
+            counts[row[0]] = 1
+        else:
+            ret[row[0]] += int(row[1])
+            counts[row[0]] += 1
+    for k in ret:
+        ret[k] = ret[k] / counts[k]
+    return ret
 
-for line in open("dat/bm.dat", "r").readlines():
-    row = line.split("\t")
-    di[row[0]] = int(row[1])
+diOld = None
+if os.path.isfile("dat/bm.old.dat"):
+    diOld = readDat("dat/bm.old.dat")
 
-plt.bar(range(len(di)), di.values(), align="center")
+di = readDat("dat/bm.dat")
+
+for i, key in enumerate(di.keys()):
+    if diOld is not None:
+        plt.bar(i, diOld[key], color="red", width=0.25)
+        plt.bar(i + 0.25, di[key], color="green", width=0.25)
+    else:
+        plt.bar(i, di[key], color="green", width=0.5)
+
 plt.xticks(range(len(di)), di.keys())
 for sp in plt.gca().spines.values():
     sp.set_visible(False)
