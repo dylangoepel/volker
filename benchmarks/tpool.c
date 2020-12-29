0885f5a7a71922c16091a5c9f8a7e9be5e6c9314
Dylan Göpel <dylan.connor@t-online.de>
2020-12-29 16:45:31 +0100

    adjust tpool benchmark

--- benchmarks/tpool.c
+++ benchmarks/tpool.c
@@ -8,9 +8,7 @@
 #define NUM_THREADS 20
 #define MAX_QUEUE 40
 
-void *aufg(void *arg){
-  int a = (int) arg;
-  a++;
+void *nop(void *arg){
   return NULL;
 }
 
@@ -19,20 +17,22 @@ int main(){
   benchmark ben;
   tpool_t test;
 
-  bm_start(&ben);
-  tpool_init(&test, NUM_THREADS, MAX_QUEUE);
-  bm_end(&ben);
-  bm_write(&ben, "tpool_init(20)");
+  for(int i = 0; i < ITERATION_COUNT; ++i) {
+      bm_start(&ben);
+      tpool_init(&test, NUM_THREADS, MAX_QUEUE);
+      bm_end(&ben);
+      bm_write(&ben, "tpool_init");
 
-  bm_start(&ben);
-  for(int i = 0; i < 30; ++i){
-    tpool_add_work(&test, &aufg, (void*) i);
+      bm_start(&ben);
+      for(int i = 0; i < 30; ++i){
+        tpool_add_work(&test, &nop, NULL);
+      }
+      bm_end(&ben);
+      bm_write(&ben, "tpool_add_work");
+
+      bm_start(&ben);
+      tpool_destroy(&test, 1);
+      bm_end(&ben);
+      bm_write(&ben, "tpool_destroy");
   }
-  bm_end(&ben);
-  bm_write(&ben, "tpool_add_work(20)");
-
-  bm_start(&ben);
-  tpool_destroy(&test, 0);
-  bm_end(&ben);
-  bm_write(&ben, "tpool_destroy");
 }
