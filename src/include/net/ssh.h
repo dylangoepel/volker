2665ba54bd7053f83edd7aeda334180976ba8203
Michael Engel <miengel05@protonmail.com>
2021-03-25 10:06:34 +0100

    remove double pointer

--- src/include/net/ssh.h
+++ src/include/net/ssh.h
@@ -6,7 +6,7 @@
 
 
 
-ssh_session *conn_ssh(char **hst, char **usr, int *ver);
-bool ssh_pw(char **pw, ssh_session *session);
+ssh_session *conn_ssh(char *hst, char *usr, int *ver);
+bool ssh_pw(char *pw, ssh_session *session);
 
 #endif // SSH_H
--- src/net/ssh/ssh.c
+++ src/net/ssh/ssh.c
@@ -5,15 +5,14 @@
 #include <errno.h>
 #include "net/ssh.h"
 
-bool ssh_pw(char **pw, ssh_session *session){
-
+bool ssh_pw(char *pw, ssh_session *session){
   int c;
 
   if(session == NULL){
     return false;
   }
 
-  if((c = ssh_userauth_password(*session, NULL, *pw)) != SSH_AUTH_SUCCESS){
+  if((c = ssh_userauth_password(*session, NULL, pw)) != SSH_AUTH_SUCCESS){
 	    fprintf(stderr, "Error: %s\n", ssh_get_error(*session));
 	    return false;
   }
@@ -22,12 +21,10 @@ bool ssh_pw(char **pw, ssh_session *session){
       return true;
   }
 
-  
   return false;
-
 }
 
-ssh_session *conn_ssh(char **hst,char **usr,int *ver){
+ssh_session *conn_ssh(char *hst,char *usr,int *ver){
   ssh_session *session = malloc(sizeof(ssh_session *));
   *session = ssh_new();
   
@@ -36,13 +33,13 @@ ssh_session *conn_ssh(char **hst,char **usr,int *ver){
   }
 
   if(usr != NULL){
-    if (ssh_options_set(*session, SSH_OPTIONS_USER, *usr) < 0) {
+    if (ssh_options_set(*session, SSH_OPTIONS_USER, usr) < 0) {
       ssh_disconnect(*session);
       return NULL;
     }
   }
 
-  if (ssh_options_set(*session, SSH_OPTIONS_HOST, *hst) < 0) {
+  if (ssh_options_set(*session, SSH_OPTIONS_HOST, hst) < 0) {
     return NULL;
   }
   ssh_options_set(*session, SSH_OPTIONS_LOG_VERBOSITY, ver);
